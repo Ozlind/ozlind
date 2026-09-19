077aa31ed91294d9207a5734f82b0a2b0bec2f52
Ozlind <athulkrishna.k047@gmail.com>
2026-09-19 08:36:31 +0530

    Add files via upload

---
+++ app/api/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main style={{ minHeight: '100svh', display: 'grid', placeItems: 'center', background: '#090a0d', color: '#8d7cff', fontFamily: 'Inter,system-ui,sans-serif' }}>
+      <div style={{ fontSize: 10, letterSpacing: '.2em', fontWeight: 800 }}>OZLIND</div>
+    </main>
+  );
+}
